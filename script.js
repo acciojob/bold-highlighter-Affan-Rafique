d12d9f21349dda70e8051566bcbed43655564333
Affan-Rafique <affanrafique9@gmail.com>
2025-09-25 16:35:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,16 @@
+// Turns all <strong> elements green
 function highlight() {
-    //Write your code here
-	let bold=document.getElementsByTagName("strong");
-	for(let i=0;i<bold.length;i++)
-		{
-	bold[i].addEventListener('mouseover',()=>
-		{
-			bold[i].style.color="rgb(0, 128, 0)";
-		});
-		}
+    let boldElements = document.querySelectorAll('strong');
+    boldElements.forEach(element => {
+        element.style.color = "rgb(0, 128, 0)";
+    });
 }
 
-
+// Reverts all <strong> elements to black
 function return_normal() {
-    //Write your code here
-	let normal=document.getElementsByTagName("Strong");
-	for(let i=0;i<normal.length;i++)
-		{
-			normal[i].addEventListener('mouseout',()=>
-				{
-					normal[i].style.color="rgb(0, 0, 0)";
-				});
-		}
-
-    
+    let boldElements = document.querySelectorAll('strong');
+    boldElements.forEach(element => {
+        element.style.color = "rgb(0, 0, 0)";
+    });
 }
+
