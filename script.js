00b249b53fceb632a668d4f8f0aec4d0345d5ccb
Affan-Rafique <affanrafique9@gmail.com>
2025-09-25 16:23:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 function highlight() {
     //Write your code here
-
-
+	let bold=document.getElementsByTagName("strong");
+	for(let i=0;i<bold.length;i++)
+		{
+	bold[i].addEventListener('mouseover',()=>
+		{
+			bold[i].style.color='green';
+		});
+		}
 }
 
 
 function return_normal() {
     //Write your code here
+	let normal=document.getElementsByTagName("Strong");
+	for(let i=0;i<normal.length;i++)
+		{
+			normal[i].addEventListener('mouseout',()=>
+				{
+					normal[i].style.color='black';
+				});
+		}
 
     
 }
